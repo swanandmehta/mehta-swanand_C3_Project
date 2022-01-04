97cca12982b7442f6d4a4d3821feb5ba69b7a66b
Swanand <swanandmehta94@gmail.com>
2022-01-04 21:21:33 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -66,6 +66,35 @@ class RestaurantTest {
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+    @Test
+    public void providing_single_item_name_should_return_cost_of_single_item() {
+        restaurant = getRestaurant();
+        int cost = restaurant.getOrderCost("Sweet corn soup");
+        assertEquals(cost, 119);
+    }
+
+    @Test
+    public void providing_multiple_item_names_should_return_order_value() {
+        restaurant = getRestaurant();
+        int cost = restaurant.getOrderCost("Sweet corn soup", "Vegetable lasagne");
+        assertEquals(cost, (119 + 269));
+    }
+
+    @Test
+    public void providing_0_item_names_should_return_0() {
+        restaurant = getRestaurant();
+        int cost = restaurant.getOrderCost();
+        assertEquals(cost, 0);
+    }
+
+    @Test
+    public void providing_invalid_item_names_should_throw_exception() {
+        restaurant = getRestaurant();
+        assertThrows(itemNotFoundException.class, () -> {
+            restaurant.getOrderCost("ANCD", "XYZ");
+        });
+    }
+
     private Restaurant getRestaurant() {
         LocalTime openingTime = LocalTime.parse("10:30:00");
         LocalTime closingTime = LocalTime.parse("22:00:00");
